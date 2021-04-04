61121c8134363221be63679f6b02be99b98bd0ea
Manasamahesh <manasamahesh12621@gmail.com>
2021-04-05 01:10:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./databae.js");
 app.listen(5000,() => {
 	console.log("server has started on 5000 ");
 });
